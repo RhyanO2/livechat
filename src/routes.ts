d5636198bcff4337d97e338b9726e08b7f96d820
rhyano2 <rhyanoliveirads@gmail.com>
2026-03-05 17:56:35 -0300

    .

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,36 @@
-import { FastifyPluginAsync } from 'fastify';
 import { fastifyServer } from './server';
+import type { WebSocket } from 'ws';
+
+const clients = new Set<WebSocket>();
 
 export async function chat() {
+  fastifyServer.get('/health', (req, res) => {
+    try {
+      res.status(200).send({ message: 'All okay!' });
+    } catch (err: any) {
+      console.log(err);
+    }
+  });
+
   fastifyServer.get('/t', { websocket: true }, (socket, req) => {
+    clients.add(socket);
+
+    console.log(`Connected total: ${clients.size}`);
 
     socket.on('message', (message) => {
-      console.log(`IP: ${req.ip}: ${message.toString()}`);
+      const text = message.toString();
+      console.log(`IP: ${req.ip}: ${text}`);
     });
 
     socket.on('close', () => {
+      clients.delete(socket);
       console.log(`${req.ip} Disconnected!`);
+      console.log(`Connected total: ${clients.size}`);
+    });
+
+    socket.on('error', (err: any) => {
+      clients.delete(socket);
+      req.log.error('ERROR', err);
     });
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,20 @@ import { fastifyWebsocket } from '@fastify/websocket';
 import { chat } from './routes';
 
 export const fastifyServer = Fastify({
-  logger: true,
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
 });
 
 fastifyServer.register(fastifyWebsocket);
 fastifyServer.register(chat);
 
 fastifyServer.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
-  console.log('3000');
+  console.log({});
 });
